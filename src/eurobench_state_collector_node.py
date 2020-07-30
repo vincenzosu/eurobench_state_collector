c7f0d4077a7e71a385547be2b91ee59e80a746ad
Vincenzo Suriani <vincsur@gmail.com>
2020-07-30 18:40:15 +0200

    fix

--- src/eurobench_state_collector_node.py
+++ src/eurobench_state_collector_node.py
@@ -39,7 +39,7 @@ class eurobench_state_collector:
 
 
           # ################## where i am going to be subscribed #############
-          self.distance_sens_front_0 = rospy.Subscriber("/sensor/base_ir_front_0", Float64,
+          self.distance_sens_front_0 = rospy.Subscriber("sensor/base_ir_front_0", Float64,
           									   self.ds_callback, queue_size=1)
           
           self.distance_sens_front_1 = rospy.Subscriber("/sensor/base_ir_front_1", Float64,
@@ -176,10 +176,10 @@ def listener(self):
 #    rospy.init_node('eurobench_worldstate_provider', anonymous=True)
     image_camera = rospy.Subscriber("sensor_msgs/Image", Image, callback)
     
-    self.distance_sens0 = rospy.Subscriber("/sensor/base_ir_front_0", Float64,
+    self.distance_sens0 = rospy.Subscriber("sensor/base_ir_front_0", Float64,
           									   self.ds_callback, queue_size=1)
           
-    self.distance_sens1 = rospy.Subscriber("/sensor/base_ir_front_1", Float64,
+    self.distance_sens1 = rospy.Subscriber("sensor/base_ir_front_1", Float64,
           									   self.ds_callback, queue_size=1)
     
     print('---------- sensors reading ---------')
