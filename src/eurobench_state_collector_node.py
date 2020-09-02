c4391b609c305d65da96d49bcf03c166ec0c1d07
Vincenzo Suriani <vincsur@gmail.com>
2020-09-02 18:06:31 +0200

    fix

--- src/eurobench_state_collector_node.py
+++ src/eurobench_state_collector_node.py
@@ -173,7 +173,7 @@ class eurobench_state_collector:
         launch_file = os.path.join(rospkg.RosPack().get_path(package), 'launch', launch_file)
         
         print launch_file
-        cli_args = ['/home/mosaic/catkin_ws/src/robot/launch/id.launch','gzpose:="-x 1.0 -y 0.4 -z 0.86 -R 0.0 -P 0.0 -Y 0"']
+        cli_args = [launch_file,'gzpose:="-x 1.0 -y 0.4 -z 0.86 -R 0.0 -P 0.0 -Y 0"']
         roslaunch_args = cli_args[1:]
         roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], roslaunch_args)]
         parent = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
