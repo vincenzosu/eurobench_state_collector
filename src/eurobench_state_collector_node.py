54052fe4ea0e9c36d2a19706ce2069fc0b92251f
Vincenzo Suriani <vincsur@gmail.com>
2020-09-03 11:23:43 +0200

    fix

--- src/eurobench_state_collector_node.py
+++ src/eurobench_state_collector_node.py
@@ -167,7 +167,7 @@ class eurobench_state_collector:
         #launch_file = os.path.join(rospkg.RosPack().get_path(package), 'launch', launch_file)
         #sys.argv = [ 'door:=self_closing', 'direction:=push']
         
-        sys.argv = ['roslaunch', 'eurobench_reemc_door', 'reemc_door.launch', 'direction:=pull', 'gzpose:="-x -5.0 -y 0.4 -z 0.86 -R 0.0 -P 0.0 -Y 0"']
+        sys.argv = ['roslaunch', 'eurobench_reemc_door', 'reemc_door.launch', 'direction:=pull']
         import roslaunch
         roslaunch.main()
 
