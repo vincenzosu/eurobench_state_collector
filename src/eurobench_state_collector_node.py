b72fb7c8a77055dc7971e893e9b9d905524c2d34
Vincenzo Suriani <vincsur@gmail.com>
2020-09-03 13:47:18 +0200

    fix

--- src/eurobench_state_collector_node.py
+++ src/eurobench_state_collector_node.py
@@ -293,7 +293,7 @@ def talker(ebws):
 
         retrieveBenchmarkConfiguration(ebws)
         if (benchmarkConfigurationHasChanged(ebws)):
-            restartSim(ebws)
+            if(ebws.current_door_opening_side != None) restartSim(ebws)
 
         r.sleep()
 
@@ -402,8 +402,8 @@ def restartSim(ebws):
 #roslaunch.core.Node(package, launch_file, args='door:=simple direction:=pull gzpose:="-x 1.0 -y 0.4 -z 0.86 -R 0.0 -P 0.0 -Y 0"')                 
     launch_file = os.path.join(rospkg.RosPack().get_path(package), 'launch', launch_file)
     sys.argv = [ 'door:=simple', arg1]
-    self.launch = roslaunch.parent.ROSLaunchParent(uuid, [launch_file])
-    self.launch.start()
+    ebws.launch = roslaunch.parent.ROSLaunchParent(uuid, [launch_file])
+    ebws.launch.start()
 
     
 
